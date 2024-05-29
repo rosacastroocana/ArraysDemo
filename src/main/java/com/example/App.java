349506813d87668ba3275e9cbac1dfed8fe21269
rosacastroocana <rosa.castro.ocana@gmail.com>
2024-05-29 14:46:00 +0200

    sentencia for clasica

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -30,6 +30,47 @@ public class App
     	// la variante no sirve. La vaiante 2 implica utilizar una sentencia de control
     	// de fuljo para recorrer el array y lo veremos en la rama "sentenciasDeControlDeFlujo"
     	
+// Pimero: Con una sentencia for de toda la vida o clasica  
+// declarar una variable de cotrol: int i = 0; la i viend de index, también puede ser int index = 0
+//empezamos en cero que es Tamara
+// nombres.length devuelve la cantidad de elementos de nombmre
+// i++ o ++i es para que no entre en bucle y empiece a sumar uno hasta que llegue al final
+    	
+//    	for(int i = 0; i < nombres.length; i++) {
+//    		System.out.println(nombres[i]);
+//    		}
+    	// Mostrar los nombres del array de nombres que tienen más de 4 caracteres
+    	
+    	for(int i = 0; i < nombres.length; i++) {
+    		if (nombres[i].length() > 4)
+    		System.out.println(nombres[i]);
+    		}
+    	
+   
+// Que diferencia hay en utilizar el operador de autoincremento o decremento antes o dspués
+// Para solucionar esto utilizaremos JSHELL que es como un Shell de Phyton que antes Java no tenía.
+    	
+ //El operador de autoincremento si está solo en una sentencia da igual que vaya antes o después, es decir,
+ //i++; o ++i; es lo mismo, sin embargo i + ++j no es lo mismo que i + j++
+    
+ // NOTA IMPORTANTE - El for clasico se utiliza cuando nos interesa el indice. Po ejemplo si queremos recorrer 
+ // el array de números y solo quiero mostrar los elementos de indice par.
+    	
+ 
+    	for(int i = 0; i < numeros.length; i++){
+    		if(i % 2 == 0)
+    		System.out.println("El elemento de indice " + i + ", es par");
+    		
+    		
+    		
+    		
+    	}
+ // Segundo: Con un for mejorado, en ocasiones MAL llamado (porque la sentencia porEach esiste) for each (por cada):
+    	 
+    	
+    	
+
+    	
     	
     }
 }

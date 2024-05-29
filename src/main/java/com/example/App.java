433925afd0f5bafd13a9b2a03aa49f194bb67807
rosacastroocana <rosa.castro.ocana@gmail.com>
2024-05-29 11:19:42 +0200

    variante 2 de declaración y asignacion a un array

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,21 +7,20 @@ package com.example;
 public class App 
 {
     public static void main( String[] args )
+  
     {
+    	
+    	// Variante 2
         // Cómo se declara un array
     	
-    	int[] numeros; // declaramos que numeros es un array de valores enteros.
+    	int[] numeros = {
+    			1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 
+    	};
     	
-    	// Cómo agregar valores al array de números?
-    	// Primero hay que reservar memoria para los elementos del array
-    	// en este ejemplo reservamos 10 unidades:
-    	
-    	numeros = new int[10];
-    	
-    	numeros[0] = 1;
-    	numeros[1] = 2;
-    	numeros[2] = 2;
-    	
-    	// Y así hasta el indice 9, porque se empieza siempre en 0 y ya serían 10 elementos.
+    	// Un array de nombres sería
+    	String[] nombres = {
+    			"Tamara", "Jessica", "Carlos", "Adrian", "María", "Maria Jose", "Rosa", "Pablo", "Ivan", "Marly"    	
+    		   };
     }
 }
+  
